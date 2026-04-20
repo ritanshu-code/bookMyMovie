55d589e8e56c57c5766407b62ff279b3e82a8f03
ritanshu-code <lakraritanshu@gmail.com>
2026-04-20 16:16:28 +0530

    socket url updated

--- book_my_movie/src/utils/socket.js
+++ book_my_movie/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
-    withCredentials: true,
-})
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+  withCredentials: true,
+});
